85906c5907f3b3fa23abb856c9bc06cf794125a4
yutami <sabacan623@gmail.com>
2022-11-16 00:41:34 +0900

    sample03

---
+++ 03_ejs/app.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const ejs = require('ejs');
+
+// ファイル
+const index_page = fs.readFileSync('./index.ejs', 'utf8')
+
+// サーバー情報
+const hostname = '127.0.0.1'; // localhost
+const port = 3000;
+
+const server = http.createServer(getFromClient);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// ここまでメインプログラム========
+
+// createServerの処理
+function getFromClient(request, response) {
+    var content = ejs.render(index_page, {
+        title: "Indexページ",
+        content: "これはテンプレートを使ったサンプルページです。",
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(content);
+    response.end();
+}
+
