b5656e829bbc45f4681c6cbd868ad25023b7bea6
yutami <sabacan623@gmail.com>
2022-12-31 22:45:52 +0900

    セッション

---
+++ 19_expr-session/routes/hello.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+// ここには相対的なルーティングを書く（下は/hello/にアクセスした時の処理）
+router.get('/', (req, res, next) => {
+    var msg = '※何か書いて送信してください。';
+    if (req.session.message != undefined) {
+        msg = `Last Message: ${req.session.message}`;
+    }
+    var data = {
+        title: 'Hello!',
+        content: msg
+    };
+    res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+    var msg = req.body['message'];
+    req.session.message = msg;
+    var data = {
+        title: 'Hello',
+        content: `Last Message: ${req.session.message}`
+    };
+    res.render('hello', data);
+});
+
+module.exports = router;
