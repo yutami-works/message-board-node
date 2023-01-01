00e6b40b4e31622a3f7bca6ccc93952ccdb304e8
yutami <sabacan623@gmail.com>
2023-01-01 10:46:37 +0900

    インデント修正

---
+++ 01_HelloWorld/README.md
@@ -0,0 +1,3 @@
+```shell
+$ node app.js
+```
--- 01_HelloWorld/app.js
+++ 01_HelloWorld/app.js
@@ -4,11 +4,11 @@ const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("It' a true world...");
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end("It' a true world...");
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
