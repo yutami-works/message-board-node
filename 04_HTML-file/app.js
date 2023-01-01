650349cc1d61447ec3763e9703e90682938f54e7
yutami <sabacan623@gmail.com>
2023-01-01 12:42:55 +0900

    モジュールを管理から除外

--- 04_HTML-file/app.js
+++ 04_HTML-file/app.js
@@ -4,17 +4,19 @@ const fs = require('fs');
 const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
-// createServerの処理（関数宣言と違ってここに書かないとエラーになる）
-const getFromClient = (req, res) => {
-  fs.readFile('./index.html', 'UTF-8', (error, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
-};
-
 const server = http.createServer(getFromClient);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+// 関数宣言（アローと違って後ろに書いても定義エラーにならない）
+
+// createServerの処理
+function getFromClient(req, res) {
+  fs.readFile('./index.html', 'UTF-8', (error, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}
--- 05_ejs/app.js
+++ 05_ejs/app.js
@@ -9,8 +9,16 @@ const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
+const server = http.createServer(getFromClient);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// 関数宣言（アローと違って後ろに書いても定義エラーにならない）
+
 // createServerの処理
-const getFromClient = (req, res) => {
+function getFromClient(req, res) {
   // ejsをHTMLにレンダリング
   var content = ejs.render(index_page, {
     title: "Indexページ",
@@ -19,10 +27,4 @@ const getFromClient = (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(content);
   res.end();
-};
-
-const server = http.createServer(getFromClient);
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+}
