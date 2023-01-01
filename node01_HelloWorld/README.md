5603e22f0b48e80dfa43ea79e51f01099de24364
yutami <sabacan623@gmail.com>
2023-01-01 11:18:46 +0900

    配置変更

--- node01_HelloWorld/README.md
+++ node01_HelloWorld/README.md
@@ -1,3 +1,7 @@
+## Hello World
+公式サイトにある画面にHello World出すだけのやつ
+https://nodejs.org/en/about/
+
 #### 1.アプリケーションの実行
 ```shell
 $ node app.js
--- node01_HelloWorld/app.js
+++ node01_HelloWorld/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end("It' a true world...");
+  res.end("it' a true world...");
 });
 
 server.listen(port, hostname, () => {
