63a866c04a0165f88cc68c7f1d983384bf0e19b2
yutami <sabacan623@gmail.com>
2023-01-01 12:26:27 +0900

    コメント追加

--- node05_ejs/app.js
+++ node05_ejs/app.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
 
-// ファイル
-const index_page = fs.readFileSync('./index.ejs', 'utf8')
+// 同期で読み込み
+const index_page = fs.readFileSync('./index.ejs', 'utf8');
 
 // サーバー情報
 const hostname = '127.0.0.1'; // localhost
@@ -11,6 +11,7 @@ const port = 3000;
 
 // createServerの処理
 const getFromClient = (req, res) => {
+  // ejsをHTMLにレンダリング
   var content = ejs.render(index_page, {
     title: "Indexページ",
     message: "これはテンプレートを使ったサンプルページです。",
@@ -18,7 +19,7 @@ const getFromClient = (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(content);
   res.end();
-}
+};
 
 const server = http.createServer(getFromClient);
 
