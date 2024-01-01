6b9e5a3fc453916529a7f7c7f5cd4c195930cef5
yutami <sabacan623@gmail.com>
2024-01-02 01:46:54 +0900

    ejsの使用

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 // モジュールのロード
 const http = require('node:http');
 const fs = require('node:fs');
+const ejs = require('ejs');
 
 // サーバー情報定義
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// テンプレートロード
+const indexPage = fs.readFileSync('./index.ejs', 'utf-8');
+
 // アクセス時のコールバック関数
 const getFromClient = (req, res) => {
-  fs.readFile('./index.html', 'utf-8', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.end();
+  // テンプレートに値をレンダリング
+  const content = ejs.render(indexPage, {
+    title: 'indexページ',
+    content: 'これはテンプレートのサンプルページです。'
   });
+
+  // レスポンス
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(content);
+  res.end();
 }
 
 // サーバー作成
